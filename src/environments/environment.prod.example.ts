c1c3b880a490f0615fd4ae864b5163a49b313963
Humberto Maquilon <maquilon@gmail.com>
2018-04-03 07:09:31 -0400

    Initial commit

---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    authDomain: "aaaaaaaaaa.firebaseapp.com",
+    databaseURL: "https://aaaaaaaaaa.firebaseio.com",
+    projectId: "aaaaaaaaaa",
+    storageBucket: "",
+    messagingSenderId: "000000000000"
+  }
+};
