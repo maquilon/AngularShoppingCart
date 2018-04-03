2ea4c85d50496586592740adf0fdbd66391e6dc3
Humberto Maquilon <maquilon@gmail.com>
2018-04-03 07:38:14 -0400

    Added auth service

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,5 @@
-import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import * as firebase from 'firebase';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -9,14 +7,12 @@ import * as firebase from 'firebase';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent   {
-  user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) { 
-    this.user$ = afAuth.authState;
+  constructor(public auth: AuthService) { 
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+   this.auth.login();
   }
 
 }
