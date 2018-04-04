485e23910f6c09b753bcbb405d585118f9d45ff1
Humberto Maquilon <maquilon@gmail.com>
2018-04-04 16:31:17 -0400

    add filter to show all categories"

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -17,17 +18,29 @@ export class ProductsComponent {
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService, categoryService: CategoryService) { 
-    productService.getAll().subscribe(products => this.products = products);
+    productService: ProductService,
+    categoryService: CategoryService) {
+
+    productService
+      .getAll()
+      .switchMap(products => {
+        this.products = products;
+        return route.queryParamMap;
+      })
+
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+
+      });
+
+
     this.categories$ = categoryService.getAll();
 
-    route.queryParamMap.subscribe(params => {
-      this.category = params.get('category');
-      this.filteredProducts = (this.category) ?
-        this.products.filter(p => p.category === this.category) :
-        this.products;
 
-    });
   }
 
 
