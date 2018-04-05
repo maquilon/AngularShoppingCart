98f780ce84516c535157849c3dbdad21770958ac
Humberto Maquilon <maquilon@gmail.com>
2018-04-05 14:14:40 -0400

    Displaying number of products on navbar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent   {
-  appUser: AppUser;
 
-  constructor(private auth: AuthService) { 
-    auth.appUser$.subscribe(appUser => this.appUser = appUser );
+export class BsNavbarComponent implements OnInit {
+  appUser: AppUser;
+  shoppingCartItemCount: number;
+
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) {
   }
 
   logout() {
-   this.auth.logout();
+    this.auth.logout();
+  }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    let cart$ = await this.shoppingCartService.getCart();
+
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items)
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+    })
   }
 
 }
