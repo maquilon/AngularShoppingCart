fc77e34913d95ff43eeffab3df261b3f9e5eb969
Humberto Maquilon <maquilon@gmail.com>
2018-04-05 10:34:55 -0400

    Adding products to shopping cart

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -14,18 +14,7 @@ export class ProductCardComponent {
   constructor(private shoppingCartServices: ShoppingCartService) { }
 
   addToCart(product: Product) {
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      this.shoppingCartServices.create()
-        .then(result => {
-          localStorage.setItem('cartId', result.key);
-
-          // Add product to cart
-        })
-    } else {
-      // Add product to cart
-    }
-
+    this.shoppingCartServices.addToCart(product);
   }
 
 }
